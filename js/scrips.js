e3c1610ea2503deeefbba0dc1710e72f5bc144d1
dixsonhoepp <46636995+dixsonhoepp@users.noreply.github.com>
2019-01-12 20:12:11 -0400

    1
    1

---
+++ js/scrips.js
@@ -0,0 +1,11 @@
+document.getElementById("servicio").style.display = "none"
+
+function inicio(){
+	document.getElementById("informacion").style.display = "block"
+	document.getElementById("servicio").style.display = "none"
+}
+
+function servicio(){
+	document.getElementById("servicio").style.display = "block"
+	document.getElementById("informacion").style.display = "none"
+}

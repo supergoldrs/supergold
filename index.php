8cc84b93410c0b511402daa4edcea3c74c30e9ba
dixsonhoepp <46636995+dixsonhoepp@users.noreply.github.com>
2019-01-12 20:08:15 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>SuperGold</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+
+	<section class="contenedor">
+
+		<header>
+			<div class="logo">
+				<img src="img/LogoSG.png">
+			</div>
+
+			<div class="opciones">
+
+				<ul>
+					<li onclick="inicio()">Inicio</li>
+					<li onclick="servicio()">Servicios</li>
+					<li>
+						<a href="https://www.facebook.com/pg/SuperGold-1953616664934070/reviews/?ref=page_internal" target="_blank">Opiniones de clientes</a></li>
+				</ul>
+			</div>
+		</header>
+
+		<section class="img-presentacion">
+
+			<div class="informacion" id="informacion">
+
+				<h1>SuperGold</h1>
+
+				<p>Te damos la Bienvenida a nuestra página…</p>
+
+				<p>Te estaremos brindando un excelente servicio para que puedas vender el oro de:</p>
+
+				<img src="img/rs07.png" width="200px" >
+				<img src="img/rs3.png" width="200px" >
+				
+			</div>
+
+			<div class="servicio" id="servicio">
+
+				<h1>SuperGold</h1>
+
+				<p>SuperGOLD te hace saber que el objetivo principal de esta página es brindar un excelente servicio para que puedas vender tu oro con toda confianza.</p>
+				<p>Ofrecemos una excelente taza para que te sientas conforme con tu venta.</p> 
+				<p>Únicamente los estaremos atendiendo desde nuestro sitio oficial, Jamás le pediremos los datos personales de su cuenta. </p>
+				<p>Después de realizar la transacción correspondiente, se le solicitara que envié la información de su banco, para que de esa manera nuestros agentes puedan realizar el pago. </p>
+				<p>Es importante recordarle que siempre debe verificar que escribió el sitio de nuestra página web correctamente.
+				</p>
+				
+			</div>
+
+		</section>
+		
+	</section>
+
+		<!--Start of Tawk.to Script-->
+			<script type="text/javascript">
+			var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+			(function(){
+			var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+			s1.async=true;
+			s1.src='https://embed.tawk.to/5c3a44cd361b3372892fd47a/default';
+			s1.charset='UTF-8';
+			s1.setAttribute('crossorigin','*');
+			s0.parentNode.insertBefore(s1,s0);
+			})();
+			</script>
+		<!--End of Tawk.to Script-->
+
+		<script type="text/javascript" src="js/scrips.js"></script>
+
+
+</body>
+</html>
